8e800f4672e09d4c69c1c9709494a3f962dc3b57
Michael McCallum <michael@stickycode.net>
2020-06-21 22:35:17 +1200

    Add support for the flatten mapper:
    allows everything to be unpacked into the root directory

--- src/main/java/net/stickycode/plugin/shifty/FileMappers.java
+++ src/main/java/net/stickycode/plugin/shifty/FileMappers.java
@@ -4,22 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.codehaus.plexus.components.io.filemappers.FileMapper;
+import org.codehaus.plexus.components.io.filemappers.FlattenFileMapper;
 
 public class FileMappers {
 
   private String[] stripprefixes;
 
-  public String[] getStripPrefixes() {
-    return stripprefixes;
-  }
-
-  public void setStripPrefixes(String[] stripPrefixes) {
-    this.stripprefixes = stripPrefixes;
-  }
+  private boolean flatten;
 
   public FileMapper[] build() {
     List<FileMapper> mappers = new ArrayList<>();
     addStripPrefixes(mappers);
+
+    if (flatten)
+      mappers.add(new FlattenFileMapper());
+
     return mappers.toArray(new FileMapper[0]);
   }
 
@@ -28,5 +27,21 @@ public class FileMappers {
       for (String prefix : stripprefixes) {
         mappers.add(new StripPrefixMapper(prefix));
       }
+  }
+
+  public boolean isFlatten() {
+    return flatten;
+  }
+
+  public void setFlatten(boolean flatten) {
+    this.flatten = flatten;
+  }
+
+  public String[] getStripPrefixes() {
+    return stripprefixes;
+  }
+
+  public void setStripPrefixes(String[] stripPrefixes) {
+    this.stripprefixes = stripPrefixes;
   }
 }
