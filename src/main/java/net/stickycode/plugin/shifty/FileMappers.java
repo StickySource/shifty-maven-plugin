fbbffb9326afdd5c32568bd278ee55eda500e6e7
Michael McCallum <michael@stickycode.net>
2022-09-22 23:14:21 +1200

    Adding a merge mapper to put all the files into one file:
    only problem is doesn't seem to extract anything from zips

--- src/main/java/net/stickycode/plugin/shifty/FileMappers.java
+++ src/main/java/net/stickycode/plugin/shifty/FileMappers.java
@@ -5,12 +5,15 @@ import java.util.List;
 
 import org.codehaus.plexus.components.io.filemappers.FileMapper;
 import org.codehaus.plexus.components.io.filemappers.FlattenFileMapper;
+import org.codehaus.plexus.components.io.filemappers.MergeFileMapper;
 
 public class FileMappers {
 
   private String[] stripprefixes;
 
   private boolean flatten;
+
+  private String merge;
 
   public FileMapper[] build() {
     List<FileMapper> mappers = new ArrayList<>();
@@ -19,7 +22,18 @@ public class FileMappers {
     if (flatten)
       mappers.add(new FlattenFileMapper());
 
+    mergeMapper(mappers);
+
     return mappers.toArray(new FileMapper[0]);
+  }
+
+  private void mergeMapper(List<FileMapper> mappers) {
+    if (merge == null)
+      return;
+
+    MergeFileMapper mapper = new MergeFileMapper();
+    mapper.setTargetName(merge);
+    mappers.add(mapper);
   }
 
   private void addStripPrefixes(List<FileMapper> mappers) {
